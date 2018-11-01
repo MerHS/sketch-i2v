83c9008fcc38736b2b4c70a21d4a7ef44cb90c71
MerHS <starvessel@naver.com>
2018-11-01 12:25:19 +0900

    change pseudorandom

--- model/datasets.py
+++ model/datasets.py
@@ -4,8 +4,11 @@ from PIL import Image
 from torchvision import transforms
 from torch.utils.data.dataset import Dataset  # For custom datasets
 from sketchify.sketchify import blend_xdog_and_sketch, add_intensity
-def pseudorand_by_id(id):
 
+def pseudo_uniform(id, a, b):
+    return (((id * 1.253 + a * 324.2351 + b * 534.342) * 20147.2312369804) + 0.12949) % (b - a) + a
+def real_uniform(id, a, b):
+    return uniform(a, b)
 
 class SketchDataset(Dataset):
     def __init__(self, image_dir_path, file_id_list, tag_class_list, transform=None, is_train=True):
@@ -15,6 +18,7 @@ class SketchDataset(Dataset):
         self.transform = transform
         self.data_len = len(file_id_list)
         self.is_train = is_train
+        self.rand_gen = real_uniform if is_train else pseudo_uniform
 
     def __getitem__(self, index):
         file_id = self.file_id_list[index]
@@ -22,18 +26,19 @@ class SketchDataset(Dataset):
         illust_path = self.image_dir_path / f"{file_id}.png"
         sketch_path = self.image_dir_path / f"{file_id}_sk.png"
 
-        blend = uniform(-0.75, 0.25)
+        rand_gen = self.rand_gen
+        blend = rand_gen(file_id, -0.75, 0.25)
         sketch = cv2.imread(str(sketch_path), cv2.IMREAD_GRAYSCALE)
         blend_img = None
         if blend > 0:
             illust = cv2.imread(str(illust_path))
-            intensity = uniform(1., 2.)
-            degamma = uniform(1./intensity, 1.)
-            k = uniform(2.0, 3.0)
-            sigma = uniform(0.35, 0.45)
+            intensity = rand_gen(file_id, 1., 2.)
+            degamma = rand_gen(file_id, 1./intensity, 1.)
+            k = rand_gen(file_id, 2.0, 3.0)
+            sigma = rand_gen(file_id, 0.35, 0.45)
             blend_img = blend_xdog_and_sketch(illust, sketch, intensity=intensity, degamma=degamma, k=k, sigma=sigma)
         else:
-            intensity = uniform(1., 1.3)
+            intensity = rand_gen(file_id, 1., 1.3)
             blend_img = add_intensity(sketch, intensity)
 
         sketch_img = Image.fromarray(blend_img)
