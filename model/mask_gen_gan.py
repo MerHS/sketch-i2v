dce34344cd4ec3de21899d488fc79ac130ef669d
MerHS <starvessel@naver.com>
2019-02-27 15:28:01 +0900

    reduce size

--- model/mask_gen_gan.py
+++ model/mask_gen_gan.py
@@ -23,7 +23,7 @@ def make_resnext_layer(block, inplanes, planes, block_count, stride=1):
     return nn.Sequential(*layers)
 
 class MaskGenerator(nn.Module):
-    def __init__(self, input_dim=1, output_dim=3, input_size=256, layers=[3, 3, 3, 3]):
+    def __init__(self, input_dim=1, output_dim=3, input_size=256, layers=[2, 2, 2]):
         super(MaskGenerator, self).__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
@@ -33,13 +33,11 @@ class MaskGenerator(nn.Module):
         self.conv2 = self._make_encoder_block(32, 64)
         self.conv3 = self._make_encoder_block(64, 128)
         self.conv4 = self._make_encoder_block(128, 256)
-        self.conv5 = self._make_encoder_block(256, 512)
         
-        self.deconv1 = self._make_block(512, 256, layers[0])
-        self.deconv2 = self._make_block(256 + 256, 128, layers[1])
-        self.deconv3= self._make_block(128 + 128, 64, layers[2])
-        self.deconv4 = self._make_block(64 + 64, 32, layers[3])
-        self.deconv5 = nn.Sequential(
+        self.deconv1 = self._make_block(256, 128, layers[0])
+        self.deconv2 = self._make_block(128 + 128, 64, layers[1])
+        self.deconv3= self._make_block(64 + 64, 32, layers[2])
+        self.deconv4 = nn.Sequential(
             nn.Conv2d(32 + 32, 32, 3, 1, 1),
             nn.LeakyReLU(0.2),
             nn.Conv2d(32, self.output_dim, 3, 1, 1),
@@ -83,22 +81,18 @@ class MaskGenerator(nn.Module):
         out2 = self.conv2(out1)
         out3 = self.conv3(out2)
         out4 = self.conv4(out3)
-        out5 = self.conv5(out4)
 
         # ==============================
-        out4_prime = self.deconv1(out5)
+        out4_prime = self.deconv1(out4)
 
-        concat_tensor = torch.cat([out4_prime, out4], 1)
+        concat_tensor = torch.cat([out4_prime, out3], 1)
         out3_prime = self.deconv2(concat_tensor)
 
-        concat_tensor = torch.cat([out3_prime, out3], 1)
+        concat_tensor = torch.cat([out3_prime, out2], 1)
         out2_prime = self.deconv3(concat_tensor)
 
-        concat_tensor = torch.cat([out2_prime, out2], 1)
-        out1_prime = self.deconv4(concat_tensor)
-
-        concat_tensor = torch.cat([out1_prime, out1], 1)
-        full_output = self.deconv5(concat_tensor)
+        concat_tensor = torch.cat([out2_prime, out1], 1)
+        full_output = self.deconv4(concat_tensor)
 
         return full_output
 
