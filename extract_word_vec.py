db2e734b516c6319b6473a7b9d349600f70dfe44
hyunsu <gustnxodjs@gmail.com>
2019-03-04 10:22:39 +0000

    extract natural sentence from tag

--- extract_word_vec.py
+++ extract_word_vec.py
@@ -15,6 +15,58 @@ color_variant_files = ['body_lower.txt', 'body_upper.txt', 'body_whole.txt', 'fa
 
 TAG_FILE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'taglist', 'tag_dump.pkl')
 
+def split_cvt_to_words(tag_name):
+    
+    temp1 = tag_name.split('_')
+
+    temp2 = []
+
+    if '-' in temp1[0]:
+        temp2 = temp1[0].split('-')
+
+    return temp2 + temp1
+
+def generate_NL_sentence(this_image_tags, sections):
+
+    section_1 = []
+    section_2 = []
+    section_3 = []
+    
+    for tag_name in this_image_tags:
+        if tag_name in sections['section_1']: # hair, face
+            section_1.append(tag_name)
+        elif tag_name in sections['section_2']: # body
+            section_2.append(tag_name)
+        elif tag_name in sections['section_3']: # background
+            section_3.append(tag_name)
+
+    result = ['a', 'girl']
+
+    if len(section_1) > 0:
+        result.append('with')
+        for t in section_1:
+            result += split_cvt_to_words(t)
+            result.append('and')
+        result = result[:-1]
+
+    if len(section_2) > 0:
+        result.append('wearing')
+        for t in section_2:
+            result += split_cvt_to_words(t)
+            result.append('and')
+        result = result[:-1]
+
+    if len(section_3) > 0:
+        result.append('in')
+        for t in section_2:
+            result += split_cvt_to_words(t)
+            result.append('and')
+        result = result[:-1]
+
+    # a girl with (green hair) and (green eyes) wearing (~~~) and (~~~) in ~~ background
+
+    return result
+
 def load_vectors(fname, total_name_list):
     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
     n, d = map(int, fin.readline().split())
@@ -44,20 +96,20 @@ def get_tag_id(tag_dump_path):
     except EnvironmentError:
         raise Exception(f'{tag_dump_path} does not exist. You should make tag dump file using taglist/tag_indexer.py')
 
+    # print(name_to_id, cv_tag_list) # 'in_tree' = 213242, [123,234,23434,3244234]
+    return name_to_id, cv_tag_list
 
-    return name_to_id
+def make_dataset(data_dir_path, cvt_name_list, data_size=0):
 
-def image_tags(data_dir_path, cvt_name_list, data_size=0):
-    name_to_id = get_tag_id(TAG_FILE_PATH)
+    name_to_id, cv_tag_list = get_tag_id(TAG_FILE_PATH)
     
     cv_dict_name_to_id = dict()
 
     for tag_name, tag_id in name_to_id.items():
-        if tag_name in cvt_name_list:
+        if tag_id in cv_tag_list:
             cv_dict_name_to_id[tag_name] = tag_id
 
-
-    print('reading tagline', len(cv_dict_name_to_id))
+    print('reading tagline')
     rgb_train_path = data_dir_path / "rgb_train"
     rgb_test_path = data_dir_path / "benchmark"
     sketch_dir_path_list = ["keras_train", "simpl_train", "xdog_train"]
@@ -65,30 +117,33 @@ def image_tags(data_dir_path, cvt_name_list, data_size=0):
     sketch_train_path = data_dir_path / "keras_test"
     tag_path = data_dir_path / "tags.txt"
 
-    (train_id_list, train_cv_class_list) = read_tagline_txt(
+
+    train_file_id_to_sentence = read_tagline_txt(
         tag_path, rgb_train_path, cv_dict_name_to_id, data_size=data_size)
 
-    (test_id_list , test_cv_class_list) = read_tagline_txt(
+    test_file_id_to_sentence = read_tagline_txt(
         tag_path, rgb_test_path, cv_dict_name_to_id, data_size=100)
 
-    print(train_id_list[0], train_cv_class_list[0])
+    print(train_file_id_to_sentence, test_file_id_to_sentence)
+    return train_file_id_to_sentence, test_file_id_to_sentence
+    # we have to change word vector index from NL sentence
 
 def read_tagline_txt(tag_txt_path, img_dir_path, cv_dict_name_to_id, data_size=0, is_train=True):
 
-    print(cv_dict_name_to_id)
+
     cv_class_len = len(cv_dict_name_to_id)
-    print("read_tagline_txt! We will use %d tags" % (cv_class_len))
+
+    cv_dict_id_to_name = {y:x for x,y in cv_dict_name_to_id.items()}
+
+    print("read_tagline_txt! We will use %d CVT tags" % (cv_class_len))
     # tag one-hot encoding + 파일 있는지 확인
     if not tag_txt_path.exists():
         raise Exception(f'tag list text file "{tag_txt_path}" does not exist.')
 
     
+    cv_tag_set = set(cv_dict_name_to_id.values()) # 1423123, 213452,///
 
-    cv_dict_id_to_name = {y:x for x,y in cv_dict_name_to_id.items()}
-    cv_tag_set = set(cv_dict_name_to_id.values())
-
-    cv_class_list = []
-    file_id_list = []
+    file_id_to_sentence = dict()
 
     data_limited = data_size != 0
     count = 0
@@ -102,6 +157,11 @@ def read_tagline_txt(tag_txt_path, img_dir_path, cv_dict_name_to_id, data_size=0
     eye_tags = [10959, 8526, 16578, 10960, 15654, 89189, 16750, 13199, 89368, 95405, 89228, 390186]
 
 
+    sections = make_sections()
+
+
+    # print(sections)
+
     with tag_txt_path.open('r') as f:
         for line in f:
             count_all += 1
@@ -110,10 +170,10 @@ def read_tagline_txt(tag_txt_path, img_dir_path, cv_dict_name_to_id, data_size=0
             tag_list = set(tag_list[1:])
 
 
-            if not (img_dir_path / f'{file_id}.png').exists():
-                continue
+            # if not (img_dir_path / f'{file_id}.png').exists():
+            #     continue
 
-            print(file_id, tag_list)
+            # print(file_id, tag_list)
             
             # For face images
             # if len(tag_list) < 8:
@@ -134,36 +194,80 @@ def read_tagline_txt(tag_txt_path, img_dir_path, cv_dict_name_to_id, data_size=0
 
             for tag in tag_list:
                 if tag in cv_tag_set:
-                    this_image_tags.append(tag)
+                    this_image_tags.append(cv_dict_id_to_name[tag])
                     tag_exist = True
                     cv_tag_num += 1
             if not tag_exist:
                 continue
 
-            final_cv_class = generate_NL_sentence(this_image_tags)
+            # print(this_image_tags)
 
-            file_id_list.append(file_id)
-            cv_class_list.append(final_cv_class)
+            sentence = generate_NL_sentence(this_image_tags, sections) # [red_eyes, blue_hair]
+
+            # print(sentence)
+
+
+            # sentence : a girl with ....
+            
+            file_id_to_sentence[file_id] = sentence
 
             all_tag_num += len(tag_list)
             count += 1
             if data_limited and count > data_size:
                 break
 
+    # print(file_id_to_sentence)
     print(f'count_all {count_all}, select_count {count}, awful_count {awful_tag_num}, all_tag_num {all_tag_num},  cv_tag_num {cv_tag_num}')
-    return (file_id_list, cv_class_list)
+    return file_id_to_sentence
 
+def make_sections():
+    
+    color_variant_files = ['body_lower.txt', 'body_upper.txt', 'body_whole.txt', 'face.txt', 'hair.txt', 'background.txt']
+
+    section_1_files = ['face.txt', 'hair.txt']
+    section_2_files = ['body_lower.txt', 'body_upper.txt', 'body_whole.txt']
+    section_3_files = ['background.txt']
+
+
+    sections = dict()
+    sections['section_1'] = []
+    sections['section_2'] = []
+    sections['section_3'] = []
+
+    # print("----CVT sections----")
+    for fn in section_1_files:
+        with open('taglist/CVT/' + fn, 'r') as f:
+            for line in f:
+                tag_line = line.split()
+                tag_id, tag_name = int(tag_line[0]), tag_line[1]
+                sections['section_1'].append(tag_name)
+
+    for fn in section_2_files:
+        with open('taglist/CVT/' + fn, 'r') as f:
+            for line in f:
+                tag_line = line.split()
+                tag_id, tag_name = int(tag_line[0]), tag_line[1]
+                sections['section_2'].append(tag_name)
+
+    for fn in section_3_files:
+        with open('taglist/CVT/' + fn, 'r') as f:
+            for line in f:
+                tag_line = line.split()
+                tag_id, tag_name = int(tag_line[0]), tag_line[1]
+                sections['section_3'].append(tag_name)                
+
+    return sections
 
 if __name__ == '__main__':
     file_name = 'wiki-news-300d-1M.vec'
 
-    cvt_name_list = []
+    word_list = []
 
     extra_words = ['a', 'girl', 'boy', 'with', 'and', 'wearing']
 
     
 
-    print("----CVT----")
+    # print("----CVT----")
     for fn in color_variant_files:
         with open('taglist/CVT/' + fn, 'r') as f:
             for line in f:
@@ -172,32 +276,47 @@ if __name__ == '__main__':
                 tag_names = tag_name.split('_')
 
                 for t in tag_names:
-                    if t in cvt_name_list:
-                        pass
-                    else:
-                        cvt_name_list.append(t)
-    
-    total_name_list = cvt_name_list + extra_words
+                    if '-' in t:
+                        t_sub = t.split('-')
+                        # print(t_sub)
 
-    print(len(total_name_list))
+                        for temp in t_sub:
+                            if temp in word_list:
+                                pass
+                            else:
+                                word_list.append(temp)
+                    else:                        
+                        if t in word_list:
+                            pass
+                        else:
+                            word_list.append(t)
+        
+    total_name_list = word_list + extra_words
+
+    # print(len(total_name_list))
 
     data = load_vectors(file_name, total_name_list)
 
-    print(len(data))
+    # print(len(data))
 
     data_keys = list(data.keys())
 
     not_included_words = set(total_name_list) - set(data_keys)
 
-    print(not_included_words)
+    # print(not_included_words)
 
 
 
-    # torch.save({
-    #     'word_names' : data_keys,
-    #     'word_vectors' : data
-    #     }, os.path.join('./', 'w2v.pkl'))
+    torch.save({
+        'word_names' : data_keys,
+        'word_vectors' : data
+        }, os.path.join('./', 'w2v.pkl'))
 
 
     data_dir_path = Path('../dataset/')
-    image_tags(data_dir_path, cvt_name_list)
+    train_file_id_to_sentence, test_file_id_to_sentence = make_dataset(data_dir_path, word_list)
+
+    torch.save({
+        'train' : train_file_id_to_sentence,
+        'test' : test_file_id_to_sentence
+        }, os.path.join('./', 'tag2sentence.pkl'))
